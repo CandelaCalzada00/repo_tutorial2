5af73fe24ac4cb4803c18a8fb763ad9d7502b05a
CandelaCalzada00 <candelacalzada00@gmail.com>
2026-03-03 16:37:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-apellido = input("Ingresa tu apellido: ")
-print(f"Bienvenid@ {apellido}")
+
+print("Hola mundo desde Main")
+
+numero = int(input("Ingrese un nro: "))
+numero2 = int(input("Ingrese otro nro: "))
+
+print(f"Resultado de suma es: {numero + numero2}")
+
